0a1d64865c75bf85cdafa88024fdf6fbd3dfb73a
LittlefieldBous <amybousamra.com>
2014-01-11 10:32:38 -0700

    commit of associating js

---
+++ associatingJS/js/script.js
@@ -0,0 +1,15 @@
+//JavaScript
+
+var yearBorn = 1955;
+
+console.log("Hello World");
+console.log(yearBorn);
+
+alert("ping-pong");
+alert (yearBorn);
+
+var firstNumber = 3;
+var secondNumber = 3;
+var answer;
+answer = 3 * 3;
+alert (3 * 3);
