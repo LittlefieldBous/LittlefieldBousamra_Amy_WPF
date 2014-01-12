d79fa5c1817c50039e979a3b5c2b71ec7e2d1e69
LittlefieldBous <amybousamra.com>
2014-01-12 10:56:18 -0700

    initial commit Expressions Worksheet
    javascript exercises

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,29 @@
+//JavaScript
+
+//Amy Littlefield-Bousamra 
+// 1/12/2014
+//Expressions Worksheet
+
+
+//Example area of a Rectangle
+var width = 3;
+var length= 6;
+// area of a rectangle = length * width
+var area = width * length;
+console.log(area);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
