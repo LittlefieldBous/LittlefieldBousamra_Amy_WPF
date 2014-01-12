d2cc69390a7c94b9cb7e9c23d2ae1488e7b09499
LittlefieldBous <amybousamra.com>
2014-01-12 14:26:22 -0700

    dog problem added
    Expression Worksheet Assignment

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -15,9 +15,19 @@ console.log(area);
 
 
 
+//Dog Years
 
 
+//calculation of actual dog age when compared to human age. one can plug in how old the dog is to find the dog's actual age. For example the dog is 2 years old. Therefore in human years the dog is actually 14 years old.
+var dogAge = "2";
+var humanAge = "7";
+var result =  dogAge * humanAge;
+var phrase1 = "Sparky is";
+var phrase2 = "human years old which is";
+var phrase3 = "in dog years.";
+var answer = String(phrase1) + " " + String(dogAge)  +" "+ String(phrase2) + " "+ String(result)+ " " + String(phrase3);
 
+console.log(answer);
 
 
 
