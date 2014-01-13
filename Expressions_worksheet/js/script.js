b8b97f93bc3bd5b54c51473a846721b01efee61c
LittlefieldBous <amybousamra.com>
2014-01-12 17:23:54 -0700

    expressions worksheet complete

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -62,9 +62,45 @@ console.log(sparkyshare);
 
 var groceryCost = [145, 150, 120, 168, 110]; //amount spent per week for five weeks on groceries. index 0-4
 
+var total = groceryCost[0] + groceryCost[1] + groceryCost[2] + groceryCost[3] + groceryCost[4];
+
 var average = (groceryCost[0] + groceryCost[1] + groceryCost[2] + groceryCost[3] + groceryCost[4])/5;
 
-console.log(average);
+var ph1 = "You have spent a total of $";
+var ph2 = "on groceries over 5 weeks.";
+var ph3 = "That is an average of $";
+var ph4 = "per week";
+
+var budget = String(ph1) + String(total) + " " + String(ph2) + " " + String(ph3) + String(average) + " " + String(ph4);
+
+console.log(budget);
+// You have spent a total of $X on groceries over 5 weeks. That is an average of $X per week”
+
+//discounts
+
+var item = "Moonboots";
+var originalPrice = "50.00";
+var percentOff = "20%";
+var percentageRate = ".20";
+var salesDiscount = percentageRate * originalPrice;
+var salesPrice = originalPrice - salesDiscount;
+var stateTax = ".06";
+var TaxTotal = salesPrice * stateTax;
+var SalesTotal = salesPrice + TaxTotal;
+
+var n1 = "Your";
+var n2 = "was originally $";
+var n3 = "but after a";
+var n4 = "discount, it is now $";
+var n5 = "without tax, and $";
+var n6 ="with tax.";
+
+//Your X was originally $X, but after a X% discount, it is now $X without tax, and $X with tax.
+
+var discount = String(n1) + " " + String(item) + " " + String(n2) + String(originalPrice) + "," +  " " + String(n3)+ " " + String(percentOff)+" " + String(n4) + (salesPrice) + " " + String(n5) + String(SalesTotal) + " " + String(n6);
+
+console.log(discount);
+
 
 
 
