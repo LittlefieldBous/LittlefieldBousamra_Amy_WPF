13f44b421b12e0c68084e17a02a85147612091a2
LittlefieldBous <amybousamra.com>
2014-01-13 09:26:09 -0700

    learning javascript prompts tp associating js file

--- associatingJS/js/script.js
+++ associatingJS/js/script.js
@@ -1,5 +1,21 @@
 //JavaScript
 
+//prompt and Alert
+// /n puts information on two lines
+
+var userInput = prompt("Enter your year of birth:");
+console.log(userInput);
+
+//prompt expression
+
+var width = prompt("We are calculating the area of rectangle. \nPlease enter the width");
+var height = prompt("Please enter the height.");
+var area = width * height; //calculates with that info
+//console.log(area);
+var result = "The area of your rectangle is"+" " + area + "sq feet";
+alert(result);
+
+
 var yearBorn = 1955;
 
 console.log("Hello World");
