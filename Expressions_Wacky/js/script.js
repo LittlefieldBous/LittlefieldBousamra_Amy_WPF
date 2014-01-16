8e7dc092f9676734abc97a071e5c52d959fc4970
LittlefieldBous <amybousamra.com>
2014-01-15 18:08:38 -0700

    added to expressions_wacky
    added functions

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,38 +5,25 @@
 //Expressions Wacky
 
 
-//var orangeBins = [234, 567, 883];
-//var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
-//console.log(total);
-
-//concatenating strings...
-
-//var firstName = "Kermit";
-//var lastName = "The Frog";
-//var fullName = firstName + " " + lastName;
-
-//console.log(fullName);
-
-//var muppetNames =["Kermit","Miss Piggy"];
-
-//muppetNames[1] = "Rizzo";
-//muppetNames[2] = "Fozzy";
-
-//var num=2;
-
-//console.log (muppetNames [num]);
-
-
 //The Chef's New Dish Name Calculator: naming a new dish based on prefixes and suffixes found in dishes.
 //found pasta endings listed in Wikapedia //Italian pasta suffixes are 0-27 Italian pasta prefixes 27-43
 
 //Array
 
-var dishNames = [ "ini", "elli", "telli", "delle", "icelli", "coti", "celloni", "illi" , "i", "etti",  "guine", "uccine", "ette", "ine", "elle", "izzoni", "loni", "zoni", "oni", "one", "otti", "ozzi", "acci", "iani", "casserole", "pie", "ragout", "moussaka", "Ba", "Big", "Bucat", "Capp", "Fett", "Fus", "Fust", "Lin", "Macar", "Mani", "Pappar", "Pell", "Perciat", "Spaghett", "Taglia", "Verm", "Zit"]
+var dishNames = [ "ini", "elli", "telli", "delle", "icelli", "coti", "celloni", "illi" , "i", "etti",  "guine", "uccine", "ette", "ine", "elle", "izzoni", "loni", "zoni", "oni", "one", "otti", "ozzi", "acci", "iani", "strussel", "casserole", "pie", "ragout", "moussaka", "tada", "Ba", "Big", "Bucat", "Capp", "Fett", "Fus", "Fust", "Lin", "Macar", "Mani", "Pappar", "Pell", "Perciat", "Spaghett", "Taglia", "Verm", "Zit", "foo"]
 
 
 var namegenerator = dishNames[44] + dishNames[2];
 console.log(namegenerator);
+
+var namegenerator = dishNames[22] + dishNames[2];
+console.log(namegenerator);
+
+var namegenerator = dishNames[38] + dishNames[5];
+console.log(namegenerator);
+
+var namegeneratorA = dishNames[47] + dishNames[24];
+console.log(namegeneratorA);
 
 var prefix = prompt ("Please enter a prefix for your dish here");
 var nameA = prefix + dishNames[1];
@@ -45,6 +32,12 @@ console.log(nameA);
 var suffix = prompt ("Please enter a suffix for your dish here");
 var nameB =  dishNames[30] + suffix;
 console.log(nameB);
+
+var firstname =  prompt ("Still cannot find a name for your deluctable dish? Combine your own prefixes and suffixes to create a name." + " "+ "Please enter a prefix for your dish here");
+
+var suffix = prompt ("Please enter a suffix for your dish here");
+var nameC =  firstname + suffix;
+console.log(nameC);
 
 
 
