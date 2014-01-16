7562c2a57d5e7f92ca2e80b4d7902f453decbffc
LittlefieldBous <amybousamra.com>
2014-01-16 12:56:58 -0700

    more calculations_industry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -8,14 +8,17 @@
 //Calculating yardage for curtains
 
 
-var measureLength = prompt("The Interior decorator's calculator to measuring curtains." + " " + "First let's measure the length." + " " + "Using your measuring tape, begin at the top of the curtain rod and stop where you would prefer the length to be." + " " + "Record the number of inches here");
+var measureLength = prompt("Hello, I am the Interior decorator's calculator to measuring curtains." + " " + "Please note:" + " " + "This calculation is for curtains without a pattern." + " " + "First let's measure the length." + " " + "Using your measuring tape, begin at the top of the curtain rod and stop where you would prefer the length to be." + " " + "Record the number of inches here." + " " + "Please enter only the numerical value.");
+
+var addedInches = prompt("Next add in additional inches you will need for the length of the curtain." + " " + "These additional inches accounts for the header allowance and hem." + " " +  "This typically ranges anywhere from 8 to 16 inches." + " " +  "If you are not certain 11 inches is a good standard." + " " + "Please enter the numberical value only." );
 
 var measureWidth = prompt("Next, measure the width." + " " + "Using your measuring tape begin from one end of the curtain rod to the other end." + " " + "You do not want to measure the window because curtains should extend beyond the window." + " " + "Record the number of inches here" + " " + "Please only put in the numerical value.");
 
 var lengthInches = parseInt(measureLength);
+var extraInches = parseInt(addedInches);
 var widthInches = parseInt(measureWidth);
 
-var length = lengthInches + 9; //traditional style add 6 inchesof fabric above and 3 inches below
+var length = lengthInches + extraInches; //traditional style add 6 inchesof fabric above and 3 inches below
 var width = (widthInches + 6) * 2; //First add 6 inches (3 inches for each side of the curtain width.) Then multiply by 2 for fullness.
 
 console.log(length);
@@ -23,18 +26,29 @@ console.log(width);
 
 //Typically a designer would add the extra amount of inches to the fabric...however these variables could be changed to fit customer preference.
 
-var summary = "Typically a designer would add a total of nine inches:" + " " +  "6 inches of fabric to the top and 3 inches to the bottom of the curtain." + " " + "This would make the length of your fabric equal" + " " + length + " " + "inches." + " " + "To calculate the correct fullness of the curtain, the width is multiplied by 2.5 and then two more inches is added for the seam." + " " + "This equals" + " " + width + "inches.";
+var summary = "The overall length of your fabric equals" + " " + length + " " + "inches." + " " + "To calculate the correct fullness of the curtain,  six additional inches are added to the width (3 inches to each side)." + " " +  "Next the width is multiplied by 2." + " " + "This equals" + " " + width + " " + "inches." + " " + "Next we will calulate the total yards needed to complete your order.";
+
 alert(summary);
 
 var finalLength = length / 36; 
-
 console.log(finalLength);
 
-var purchase = prompt ( "What is the width of the fabric the customer is planning to buy?" + " " + "Typically fabric is 54 inches." + " " + "Please enter the width of the fabric you are planning to purchase or confirm that it is 54 inches in length." + " " + "Only put in the numerical value.")
+var lengthSum = "The length is then divided by 36 to convert inches to yards." + "This will give you" + " " + finalLength + "yards.";
 
-var finalWidth = width / purchase; //divide
+alert(lengthSum);
+
+var purchase = prompt("What is the width of the fabric the customer is planning to buy?" + " " + "Typically fabric is 54 inches." + " " + "Please enter the width of the fabric you are planning to purchase or confirm that it is 54 inches in length." + " " + "Only put in the numerical value.");
+
+var orderFabric = parseInt(purchase);
+
+var finalWidth = width / orderFabric; //divide the width measured by the fabric in the factory you will be purchasing...Factory standard width is typically 54 inches.
+
 
 console.log(finalWidth);
+
+var finalYards = finalWidth * finalLength;
+
+console.log(finalYards);
 
 
 
