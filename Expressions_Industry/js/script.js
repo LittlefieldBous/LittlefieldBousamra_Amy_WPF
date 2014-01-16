cf7891f10997eab1edba01d11ef377b6984bce25
LittlefieldBous <amybousamra.com>
2014-01-15 21:05:49 -0700

    Fixed some calculations and words_industry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -12,16 +12,20 @@ var measureLength = prompt("The Interior decorator's calculator to measuring cur
 
 var measureWidth = prompt("Next, measure the width." + " " + "Using your measuring tape begin from one end of the curtain rod to the other end." + " " + "You do not want to measure the window because curtains should extend beyond the window." + " " + "Record the number of inches here");
 
-var length = measureLength + 9; //traditional style add 6 inchesof fabric above and 3 inches below
-var width = measureWidth * 2.5 + 2; //For fullness multiply by 2.5 and then add 2 inches for the seam.
+var lengthInches = parseInt(measureLength);
+var widthInches = parseInt(measureWidth);
+
+var length = lengthInches + 9; //traditional style add 6 inchesof fabric above and 3 inches below
+var width = (widthInches * 2.5) + 2; //For fullness multiply by 2.5 and then add 2 inches for the seam.
 
 console.log(length);
+console.log(width);
 
 //Typically a designer would add the extra amount of inches to the fabric...however these variables could be changed to fit customer preference.
 
-var totalInches = "Typically a designer would add 6 inches of fabric to the top, 3 inches to the bottom of the curtain and three inches to each side of the curtain." + " " + "This would make the length of your fabric equal" + " " + length + "inches." + " " + "And the width of your fabric equal" + " " + width + "inches.";
+var summary = "Typically a designer would add a total of nine inches:" + " " +  "6 inches of fabric to the top and 3 inches to the bottom of the curtain." + " " + "This would make the length of your fabric equal" + " " + length + " " + "inches." + " " + "To calculate the correct fullness of the curtain, the width is multiplied by 2.5 and then two more inches is added for the seam." + " " + "This equals" + " " + width + "inches.";
+alert(summary);
 
-alert(totalInches);
 
 
 
