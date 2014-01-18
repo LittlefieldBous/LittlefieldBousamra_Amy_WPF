b6de0a4f51ce2cac687620dfddd84b04c0850720
LittlefieldBous <amybousamra.com>
2014-01-17 19:38:28 -0700

    initial commit Conditionals
    Notes on videos, learning javascript-conditionals

---
+++ Conditionals/js/script.js
@@ -0,0 +1,60 @@
+//Javascript-Conditionals
+//Amy littlefield-Bousamra
+
+
+
+//Conditional Statement:
+//True the action is performed.
+//False the action is not performed. Then use the else function.
+
+//if(condition){
+	//actions //grouping code 
+//}
+
+
+//conditional logic: If it's sunny lets go to the beach.
+//if our sunny variable is true then we will want to go to the beach
+
+//Example:
+//sunny = true 
+//if(sunny){
+//if(condition){
+  //   goToTheBeach();
+  //   } 
+     
+     
+//if(sunny){
+//goToTheBeach();
+//}else{
+//gotoTheMovies();
+//}
+
+
+//if(temp>70){ //if the temperature is greater than 70 then we will go to the beach.
+	//goToTheBeach();
+//}
+
+//Comparison
+
+//temp=82 //temp variable True
+//temp =65 False
+//if(temp>70){
+	//goToTheBeach();
+//}
+
+
+//>Greater than
+//<less than
+//>= greater than or equal 
+//<= less than or equal to
+//!= Not equal to
+//== Equal to (do not use spaces for ==)
+
+
+
+
+
+
+
+
+
