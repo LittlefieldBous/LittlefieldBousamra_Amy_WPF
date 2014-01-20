9def9408fa218772ff692c0390e89e41c0e13ee8
LittlefieldBous <amybousamra.com>
2014-01-19 18:54:40 -0700

    conditional worksheet
    conditional worksheet 2nd problem

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -8,7 +8,6 @@
 Degrees (in F or C)
 Unit (a string holding an “F” or a “C”)
 Result:
-“The temperature is X degrees Celsius.” Or “The temperature is X degrees Fahrenheit.”
 Data Sets to Test: (Note that data sets are not the only numbers that should work with your code.)
 
 
@@ -17,6 +16,7 @@ Data Sets to Test: (Note that data sets are not the only numbers that should wor
 90F is 32.22C*/
 
 
+//An example:
 //Convert 37°C to Fahrenheit.
 //37°C x  9/5 + 32 = 98.6°F
 
@@ -30,17 +30,20 @@ var cels = "C";
 
 
 var degreeM = prompt("Do you want to convert your temperature to Celsius or Fahrenheit?" + " " + "Please enter" + " " + "C" + " " + "for Celsius and" + " " + "F" + " " +  "for Fahrenheit.");
+console.log(degreeM);
+
 var convertNum = prompt("Enter the temperature here." + " " + "Please enter a numerical value only.");
 
 var tempNum = parseInt(convertNum);
+
+console.log(tempNum);
+
 var degreeF = (tempNum * 9) /5 + 32;
 var degreeC = 5/9 * (tempNum - 32);
  
-console.log(degreeM);
-console.log(tempNum);
+
 console.log(degreeF);
 console.log(degreeC);
-
 
 
 //if the person entered C convert the number to celsius c=5/9 (F-32)
@@ -64,11 +67,51 @@ else
 
 
 
+//Hot Enough?
+//Is it hot enough to go to the beach?
+//var temp = 80;
+//if the temperature is less than 75..
+
+//if(temp < 75){
+     //if its less than 75
+    // console.log(“We will go to the beach!”);
+//}else{
+     //if it’s greater or equal to 75
+     //console.log(“We will go to the movies.”);
+//}
 
 
+/*Check the Login
 
+Make sure the user has the correct username and password. If the username doesn’t match then a specific message for that should be printed to the console. If the password doesn’t match a message should appear for that, etc. Only one error message should appear.
 
+    Given:
+        Username entered by user
+        Password entered by user
+        Correct username
+        Correct password
+    Result To Print Out:
+        “Welcome, (place their username here)!”  - if the username and password is correct
+        “User not found. Try again.” -if the username does not match
+        “Password does not match our records.”  -if the username matches but the password does not*/
 
+var userName = "Joe1977"; //username entered by user
+var password = "Mybigtoe"; //password entered by user
+
+var enterName = prompt("Enter your username here.");
+var enterPass = prompt("Enter your password here");
+
+if (enterName === userName && enterPass === password){
+	console.log("Welcome, Joe1977!");
+}
+
+if (enterName != userName){
+	console.log("User not found." + " " +"Try again.");
+}
+
+if(enterPass != password){
+	console.log("Password does not match our records.")
+}
 
 
 
