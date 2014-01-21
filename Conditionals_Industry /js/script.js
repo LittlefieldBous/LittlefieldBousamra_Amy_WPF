3a7e0c62c8a2b8570291a0754d540898c976000a
LittlefieldBous <amybousamra.com>
2014-01-20 18:44:58 -0700

    conditional industry first expression
    other updates conditional assignment

--- Conditionals_Industry /js/script.js
+++ Conditionals_Industry /js/script.js
@@ -4,6 +4,33 @@
 //Conditionals Industry
 // 1/23/2014
 
+//Start-up costs for a web design business
+
+//References used: website tutsplus 9 Steps to Starting your Freelance Web Design Business, website WebDesignerDepot Starting a Web Design Business
+
+
+var yes = "Y";
+var no = "N";
+
+var introP = prompt("Hello, I'm start-up Sal!" + " " + "I am here to help calculate start-up costs for a web design business." + " " + "We will begin with figuring out the initial start-up cost." + " " + "Will you need to purchase new software?" + " "
+ + "Please enter" + " " + "Y" + " " + "for Yes or" + " " + "N" + " " + "for No.");
+ 
+if (introP === "Y"){
+    var softwareP = prompt("Please enter the cost of any new software purchased.");
+    alert(softwareP);
+    var softwareCost = parseInt(softwareP);
+    alert("The cost of software is" + "$" + softwareCost + ".");
+      console.log(softwareCost);
+}	
+        
+else {
+   var softwareCost = 0; 
+    console.log(softwareCost);
+	
+}
+
+
+
 
 
 
--- Conditionals_Wacky /js/script.js
+++ Conditionals_Wacky /js/script.js
@@ -2,9 +2,15 @@
 
 //Amy Littlefield-Bousamra 
 // 1/19/2014
+
 //Conditionals Wacky
 
 
+//What does the fox say?
 
+//physic fox..or the 8 ball...
+
+
+//dialogue from old movies or star trek movies combined with shakespeare...
 
 
