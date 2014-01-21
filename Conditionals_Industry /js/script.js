539d1c92b2638d65678d856dca89a5a96824ae9f
LittlefieldBous <amybousamra.com>
2014-01-20 19:07:31 -0700

    industry conditionals
    more code if else statements

--- Conditionals_Industry /js/script.js
+++ Conditionals_Industry /js/script.js
@@ -12,12 +12,12 @@
 var yes = "Y";
 var no = "N";
 
-var introP = prompt("Hello, I'm start-up Sal!" + " " + "I am here to help calculate start-up costs for a web design business." + " " + "We will begin with figuring out the initial start-up cost." + " " + "Will you need to purchase new software?" + " "
+var introP = prompt("Hello, I'm start-up Sal!" + " " + "I am here to help calculate start-up costs for a web design business." + " " + " " + "Will you need to purchase new software?" + " "
  + "Please enter" + " " + "Y" + " " + "for Yes or" + " " + "N" + " " + "for No.");
+ console.log(introP);
  
 if (introP === "Y"){
     var softwareP = prompt("Please enter the cost of any new software purchased.");
-    alert(softwareP);
     var softwareCost = parseInt(softwareP);
     alert("The cost of software is" + "$" + softwareCost + ".");
       console.log(softwareCost);
@@ -25,7 +25,24 @@ if (introP === "Y"){
         
 else {
    var softwareCost = 0; 
-    console.log(softwareCost);
+   console.log(softwareCost);
+	
+}
+
+
+var introH = prompt("Will you need health insurance?" + " " + "Please enter" + " " + "Y" + " " + "for Yes or" + " " + "N" + " " + "for No.");
+ console.log(introH);
+ 
+if (introH === "Y"){
+    var healthP = prompt("Enter your monthly cost for health insurance." + " " + "Please enter the numerical cost only.");
+    var healthIns = parseInt(healthP);
+    alert("The cost of health insurance" + "$" + healthIns + ".");
+      console.log(healthIns);
+}	
+        
+else {
+   var healthIns = 0; 
+    console.log(healthIns);
 	
 }
 
