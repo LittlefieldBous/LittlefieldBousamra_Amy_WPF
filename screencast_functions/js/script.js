098862133a16552bd0aedacc6197c186f2e20dc9
LittlefieldBous <amybousamra.com>
2014-01-25 11:03:50 -0700

    initial commit screencast functions
    notes-learning web programming

---
+++ screencast_functions/js/script.js
@@ -0,0 +1,21 @@
+//JavaScript
+
+//Amy Littlefield-Bousamra
+//screencast functions
+
+
+
+//Sequential line by line, order by order -expressions
+//Conditionals: ternary, if, else, else if...
+//Reusable...chunks of code you need to reoccur again and again...
+//example cookbook how to make types of cake ...you might put how to make frosting on one page and then reference the page the frosting recipe is on..for different types of cakes.
+
+//repeating blocks of code..used to be called sub routines...mini routines..
+
+//Types of Functions
+
+//normal functions
+//procedures
+//anonymous functions
+
+
