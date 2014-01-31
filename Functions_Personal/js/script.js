0e72cd0a8327441288980756014a79d3a71f39b5
LittlefieldBous <amybousamra.com>
2014-01-30 20:04:51 -0700

    updates and changes
    more calculations

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -3,45 +3,17 @@
 //Amy Littlefield-Bousamra
 //Personal_Functions:
 
-
-//Analyzing this room according to the 60-30-10...Rule
-
-
-//Rule of third in home design...
 //Applying the golden ratio to interior design
+//30% of the room filled with furniture and 10 % accesories allowing the rest of th 60% for room space.
+//I put in the example inches as shown in the floor plan so it would be easier to type in a number and verify/check my calculations.
 
 
-
-alert("This program examines how one can apply the concept of the golden ratio when designing a room in their home."+ " " + "Utilizing 60-30-10 one must fill 30 percent of the room with furnishings, 10% with accessories and allow the rest 60% to maintain open space." + " " + "Click okay for an example and then design your own room using the 60-30-10 golden ratio.");
-
-
-// 60/30/10 can be applied to home decor in percentages. Concept: putting it to work through calculations.
-
-//
-
-//Example a living room that is 14 ft wide and 22 ft long.
-//
+//Introduction
+alert("This program examines how one can apply the concept of the golden ratio when designing a room in their home."+ " " + "Utilizing 60-30-10 one must fill 30 percent of the room with furnishings, 10% with accessories and allow the rest 60% to maintain open space." + " " + "Click okay for an example and then design your own room using the golden ratio.");
 
 
-//Color  60-30-10...
-
-//60% is the main color or dominant color
-//30% is the contrast color or secondary colot
-//10% is the accent color.
-
-
-//home furnishings
-
-////30% of the room filled with furniture and 10 % accesories allowing the rest of th 60% for room space.
-
-
-
-
-//curtains so that 60% of the window shows...measure windows.
-
-
+//Example
 //returning function
-
 
 var total = calcArea(14,22); //invoking the function (anonymous invoking is placed after function
 
@@ -69,7 +41,7 @@ console.log(total);//The total amount of squarefoot used for furnishings or acce
 alert("Next we are going to calculate our own livingroom space." + " " + "Click okay to begin.");
 
 
-// Now try measuring your livingroom...
+// 1. Now try measuring your livingroom...
 
 var w = Number(prompt("Enter the length of your livingroom using inches." + " " + "Please enter the numerical value only." + " " + "For example if your room is 22 inches in length you would enter," + " " + "22."));
 
@@ -88,12 +60,12 @@ console.log("The amount or 30% of furnishings that should be applied to this roo
 
 //Next to measure furniture...until they are less than or equal to <=r or || are  >=r + 3 
 
-//1. first or only Sofa: (35/12) * (84/12) = 21 ft Rounded
+//2. first or only Sofa: (35/12) * (84/12) = 21 ft Rounded
     
 
 var wSofa = Number(prompt("Enter the sofa's length or horizontal-side width here using inches." + " " + "Please enter the numerical value only." + " " + "For example if your sofa's length is 84 inches just type in the number," + " " + "84."));
 
-var lSofa = Number(prompt("Please start from the outer part of the sofa's seat and measure to it's back." + " " + "This is called the depth of the sofa but can also be looked at as the width." + " " + "Please enter the numerical value only." + " " +  "For example if your sofa's width is 35 inches just type in the number," + "35."));
+var lSofa = Number(prompt("Please start from the outer part of the sofa's seat and measure to it's back." + " " + "This is called the depth of the sofa but can also be looked at as the width." + " " + "Please enter the numerical value only." + " " +  "For example if your sofa's width is 35 inches just type in the number," + " " + "35."));
 
 var sofaCalc = function (wSofa,lSofa){ //defining... wSofa,lSofa, in the parameters are the storage bins  	
 var sofaSqft = Math.round(wSofa/12) * Math.round(lSofa/12);//code the function runs the width of the sofa /12 the length of the sofa/12. Next it multiplies the two to get the amount of space/sqft that the sofa will take up in the room. This is an estimate so all numbers should be rounded.
@@ -108,7 +80,7 @@ console.log("The amount of space the sofa will utilize is" + " " + s + " " + "sq
 var yes = "y";
 
 
-//2. Is there a second sofa?
+//3. Is there a second sofa?
 
 var sofaQ = prompt("Does your livingroom have another sofa or loveseat?" + " "
  + "Please enter" + " " + "y" + " " + "for Yes or" + " " + "n" + " " + "for No.");
@@ -138,7 +110,7 @@ else {
 }
 
 
-//3. will you have arm chairs and if so how many?
+//4. will you have arm chairs and if so how many?
 
 var yes = "y";
 
@@ -154,7 +126,7 @@ if (armChQ === "y"){
     var wArmChair = Number(prompt("Enter the length or horizontal-side width of one arm chair here." + " " + "Please measure using inches but enter the numerical value only." + " " + "For example if your arm chair's length is 35 inches just type in the number," + " " + "35."));
     
 
-   var lArmChair = Number(prompt("Please start from the outer part of the arm chair and measure to it's back." + " " + "This is called the depth of the sofa but can also be looked at as the width." + " " + "Please enter the numerical value only." + " " + "For example," + " " + "35."));
+   var lArmChair = Number(prompt("Please start from the outer part of the arm chair and measure to it's back." + " " + "Please enter the numerical value only." + " " + "For example," + " " + "35."));
 
    var armChairCalc = function (wArmChair,lArmChair,numChair){ //defining... the width of the armchair, the length of the arm chair and how many armchairs(numchair) in the parameters/storage bins
      	
@@ -175,14 +147,14 @@ else {
 	
 }
 
-3. //coffee tables
+//5. coffee table
 
 var yes = "y";
 
 
-//2. Is there a coffee table?
+// Is there a coffee table?
 
-var coffeeTQ = prompt("Does your livingroom have a coffee/rectangular table?" + " " + "Please do not enter side tables here that will nbe addressed in another question." +  " " + "Please enter" + " " + "y" + " " + "for Yes or" + " " + "n" + " " + "for No.");
+var coffeeTQ = prompt("Does your livingroom have a coffee/rectangular table?" + " " + "Please enter" + " " + "y" + " " + "for Yes or" + " " + "n" + " " + "for No.");
  console.log(coffeeTQ);
  
 if (coffeeTQ === "y"){
@@ -211,6 +183,85 @@ else {
 }
 
 
+ //6. bookshelf
 
+var yes = "y";
+
+
+var bookshQ = prompt("Does your living space have a bookshelf?" + " " + "Please enter" + " " + "y" + " " + "for Yes or" + " " + "n" + " " + "for No.");
+ console.log(bookshQ);
+ 
+if (bookshQ=== "y"){
+
+    var wbooksh = Number(prompt("Enter the bookshelf's width here using inches." + " " + "Please enter the numerical value only." + " " + "For example if the bookshelf's length is 13 inches just type in the number," + " " + "13."));
+
+   var lbooksh = Number(prompt("Enter the bookshelf's length here using inches." + " " + "Please enter the numerical value only." + " " +  "For example if your table's length is 35 inches just type in the number," + " " + "35."));
+
+   var bookshCalc = function (wbooksh,lbooksh){ //defining... the width of the coffee table and the length of the coffee table. (the parameters are the storage bins.)
+     	
+   var bookshSqft = Math.round(wbooksh/12) * Math.round(lbooksh/12);//code the function runs the width of the coffee table /12 the length of the coffee table/12. Next it multiplies the two to get the amount of space/sqft that the sofa will take up in the room. This is an estimate so all numbers should be rounded.
+   
+	return Math.round(bookshSqft); //what it returns //it returns what is defined in the function.
+}
+
+var bksh= ctableCalc(wbooksh,lbooksh); //invoking what happens after defining functions...The folded box is two inches wide  and 3 inches long on the horizontal side of the box.
+
+console.log("The amount of space the bookshelf will utilize is" + " " + bksh + " " + "sqft.");
+}
+        
+else {
+   var bksh = 0; //coffee table equals zero
+   alert("You do not have a bookshelf in this room.");
+   console.log(bksh); 
+	
+}
+
+//6. side tables or small accent tables
+
+var yes = "y";
+
+
+var sideTabQ = prompt("Does your living space have any additional tables?" + " " + "This could include side tables or other accent tables." + " " + "Please enter" + " " + "y" + " " + "for Yes or" + " " + "n" + " " + "for No.");
+ console.log(sideTabQ);
+ 
+if (sideTabQ === "y"){
+
+    var numsideT = Number(prompt("How many side tables will you have in your living space?"));
+
+    var wSideT = Number(prompt("Enter the tables's width here using inches." + " " + "Please enter the numerical value only." + " " + "For example if the table's length is 23 inches just type in the number," + " " + "23."));
+
+   var lSideT = Number(prompt("Enter the table's length here using inches." + " " + "Please enter the numerical value only." + " " +  "For example if your table's length is 18 inches just type in the number," + " " + "18."));
+
+   var sideTCalc = function (wSideT,lSideT,numsideT){ //defining... the width of the side table and the length of the side table. (the parameters are the storage bins.)
+     	
+   var sideTSqft = (Math.round(wSideT/12) * Math.round(lSideT/12)) * numsideT;//code the function runs the width of the side table /12 the length of the side table/12. Next it multiplies the two to get the amount of space/sqft that the side table will take up in the room. if more than one table it will multiply by 2 and so on...This is an estimate so all numbers should be rounded.
+   
+	return Math.round(sideTSqft); //what it returns //it returns what is defined in the function.
+}
+
+var sideT = sideTCalc(wSideT,lSideT,numsideT); //invoking what happens after defining functions...The folded box is two inches wide  and 3 inches long on the horizontal side of the box.
+
+console.log("The amount of space the accent or side table will utilize is" + " " + sideT  + " " + "sqft.");
+}
+        
+else {
+   var sideT  = 0; // table equals zero
+   alert("You do not have a side table in this room.");
+   console.log(sideT); 
+	
+}
+
+
+
+
+
+
+//Color  60-30-10...
+
+//60% is the main color or dominant color
+//30% is the contrast color or secondary colot
+//10% is the accent color.
+
+//curtains so that 60% of the window shows...measure windows.
 
 
