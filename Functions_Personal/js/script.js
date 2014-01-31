8b2eacdc748184629941b37723a5bbffc7beec7d
LittlefieldBous <amybousamra.com>
2014-01-30 19:02:18 -0700

    updates to personal functions
    more calculations, spacing corrections

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -12,7 +12,7 @@
 
 
 
-alert("This program examines how one can apply the concept of the golden ratio when designing a room in their home."+ " " + "Utilzing 60-30-10 one must fill 30 percent of the room with furnishings, 10% with accessories and allow the rest to maintain open space." + " " +"For an example, we will use a livingroom that is 12ft wide and 22ft long." + " " + "Click okay to view the outcome.");
+alert("This program examines how one can apply the concept of the golden ratio when designing a room in their home."+ " " + "Utilizing 60-30-10 one must fill 30 percent of the room with furnishings, 10% with accessories and allow the rest 60% to maintain open space." + " " + "Click okay for an example and then design your own room using the 60-30-10 golden ratio.");
 
 
 // 60/30/10 can be applied to home decor in percentages. Concept: putting it to work through calculations.
@@ -53,7 +53,7 @@ function calcArea(w,h){ //w,h, parameters are the storage bins //w 14 and h 22 t
 
 console.log(total);//The total amount of squarefoot used for furnishings or accessories.
 
-alert("30% of furnishings that should be applied to this room equals" + " " + total + "sqft.");
+alert("A livingroom that is 12ft wide and 22ft long has a total of 308 sqft." + " " +  "The 30% of furnishings that should be applied to this room equals" + " " + total + "sqft." + "To see a visual example please view the floor plan on this page.");
 
 
 var total = calcArea(14,22); //invoking the function (anonymous invoking is placed after function
@@ -66,14 +66,14 @@ function calcArea(w,h){ //w,h, parameters are the storage bins //w 14 and h 22 t
 
 console.log(total);//The total amount of squarefoot used for furnishings or accessories.
 
-alert("The 30% of furnishings and that should be applied to this room equals" + " " + total + "sqft." + " " + "This can also be thought of as ground cover, 10% accesories and 60% space for a more spacious non-clutter look."+ " " + "See the example on this page." + " " + "Next we are going to calculate our own livingroom space." + "Click okay to begin.");
+alert("Next we are going to calculate our own livingroom space." + " " + "Click okay to begin.");
 
 
 // Now try measuring your livingroom...
 
-var w = Number(prompt("Enter the length of your livingroom." + " " + "Please use inches as the standard of measurement." + " " + "Please enter the numerical value only" + " " + "For example if your room is 22 inches in length you would enter," + "22."));
+var w = Number(prompt("Enter the length of your livingroom using inches." + " " + "Please enter the numerical value only." + " " + "For example if your room is 22 inches in length you would enter," + " " + "22."));
 
-var l = Number(prompt("Enter the width of your livingroom using inches." + " " + "Please enter the numerical value only" + " " + "For example if your room is 14 inches wide you would enter," + "14."));
+var l = Number(prompt("Enter the width of your livingroom using inches." + " " + "Please enter the numerical value only" + " " + "For example if your room is 14 inches wide you would enter," + " " + "14."));
 
 var roomCalc = function (w,l){ //defining... wSofa,lSofa, in the parameters are the storage bins  	
 var roomSqft = (Math.round(w/12) * Math.round(l/12))* .30;//code the function runs the width of the sofa /12 the length of the sofa/12. Next it multiplies the two to get the amount of space/sqft that the sofa will take up in the room. This is an estimate so all numbers should be rounded.
@@ -91,9 +91,9 @@ console.log("The amount or 30% of furnishings that should be applied to this roo
 //1. first or only Sofa: (35/12) * (84/12) = 21 ft Rounded
     
 
-var wSofa = Number(prompt("Enter the sofa's length or horizontal-side width here using inches." + " " + "Please enter the numerical value only." + " " + "For example if your sofa's length is 84 inches just type in the number," + "84."));
+var wSofa = Number(prompt("Enter the sofa's length or horizontal-side width here using inches." + " " + "Please enter the numerical value only." + " " + "For example if your sofa's length is 84 inches just type in the number," + " " + "84."));
 
-var lSofa = Number(prompt("Please start from the outer part of the sofa's seat and measure to it's back." + " " + "This is called the depth of the sofa but can also be looked at as the width." + " " + "Please enter the numerical value only."));
+var lSofa = Number(prompt("Please start from the outer part of the sofa's seat and measure to it's back." + " " + "This is called the depth of the sofa but can also be looked at as the width." + " " + "Please enter the numerical value only." + " " +  "For example if your sofa's width is 35 inches just type in the number," + "35."));
 
 var sofaCalc = function (wSofa,lSofa){ //defining... wSofa,lSofa, in the parameters are the storage bins  	
 var sofaSqft = Math.round(wSofa/12) * Math.round(lSofa/12);//code the function runs the width of the sofa /12 the length of the sofa/12. Next it multiplies the two to get the amount of space/sqft that the sofa will take up in the room. This is an estimate so all numbers should be rounded.
@@ -106,15 +106,15 @@ console.log("The amount of space the sofa will utilize is" + " " + s + " " + "sq
 
 
 var yes = "y";
-var no = "n";
 
-//2. is there a second sofa?
+
+//2. Is there a second sofa?
 
 var sofaQ = prompt("Does your livingroom have another sofa or loveseat?" + " "
  + "Please enter" + " " + "y" + " " + "for Yes or" + " " + "n" + " " + "for No.");
  console.log(sofaQ);
  
-if (sofaQ === "y" || "Y"){
+if (sofaQ === "y"){
 
     var wSofab = Number(prompt("Enter the sofa's length or horizontal-side width here using inches." + " " + "Please enter the numerical value only." + " " + "For example if your sofa's length is 84 inches just type in the number," + "84."));
 
@@ -132,11 +132,83 @@ console.log("The amount of space the sofa will utilize is" + " " + sb + " " + "s
         
 else {
    var sb = 0; 
-   alert("You do not another sofa in this room.");
+   alert("You do not have another sofa in this room.");
    console.log(sb); 
 	
 }
 
+
+//3. will you have arm chairs and if so how many?
+
+var yes = "y";
+
+
+var armChQ = prompt("Does your livingroom have an armchair/s?" + " "
+ + "Please enter" + " " + "y" + " " + "for Yes or" + " " + "n" + " " + "for No.");
+ console.log(armChQ);
+ 
+if (armChQ === "y"){
+
+	var numChair = Number(prompt("How many arm chairs will you have in your living room?"));
+
+    var wArmChair = Number(prompt("Enter the length or horizontal-side width of one arm chair here." + " " + "Please measure using inches but enter the numerical value only." + " " + "For example if your arm chair's length is 35 inches just type in the number," + " " + "35."));
+    
+
+   var lArmChair = Number(prompt("Please start from the outer part of the arm chair and measure to it's back." + " " + "This is called the depth of the sofa but can also be looked at as the width." + " " + "Please enter the numerical value only." + " " + "For example," + " " + "35."));
+
+   var armChairCalc = function (wArmChair,lArmChair,numChair){ //defining... the width of the armchair, the length of the arm chair and how many armchairs(numchair) in the parameters/storage bins
+     	
+   var aChairSqft= Math.round((wArmChair/12) * Math.round(lArmChair/12))* numChair;//code the function runs the width of the armchair /12 the length of the armchair/12. Next it multiplies the two to get the amount of space/sqft that the armchairs will take up in the room. Then it multiplies it by the number of armchairs placed in the room. Number are rounded.
+   
+	return Math.round(aChairSqft); //what it returns
+}
+
+var ach = armChairCalc(wArmChair,lArmChair,numChair); //invoking what happens after defining functions...The folded box is two inches wide  and 3 inches long on the horizontal side of the box.
+
+console.log("The amount of squarefeet the arm chairs utilize is" + " " + ach + " " + "sqft.");
+}
+        
+else {
+   var ach = 0; 
+   alert("You do not have any arm chairs in this room.");
+   console.log(ach); //ach=armchairs
+	
+}
+
+3. //coffee tables
+
+var yes = "y";
+
+
+//2. Is there a coffee table?
+
+var coffeeTQ = prompt("Does your livingroom have a coffee/rectangular table?" + " " + "Please do not enter side tables here that will nbe addressed in another question." +  " " + "Please enter" + " " + "y" + " " + "for Yes or" + " " + "n" + " " + "for No.");
+ console.log(coffeeTQ);
+ 
+if (coffeeTQ === "y"){
+
+    var wcoffeeT = Number(prompt("Enter the table's width here using inches." + " " + "Please enter the numerical value only." + " " + "For example if your table's length is 30 inches just type in the number," + " " + "30."));
+
+   var lcoffeeT = Number(prompt("Enter the table's length here using inches." + " " + "Please enter the numerical value only." + " " +  "For example if your table's length is 48 inches just type in the number," + " " + "48."));
+
+   var ctableCalc = function (wcoffeeT,lcoffeeT){ //defining... the width of the coffee table and the length of the coffee table. (the parameters are the storage bins.)
+     	
+   var ctableSqft = Math.round(wcoffeeT/12) * Math.round(lcoffeeT/12);//code the function runs the width of the coffee table /12 the length of the coffee table/12. Next it multiplies the two to get the amount of space/sqft that the sofa will take up in the room. This is an estimate so all numbers should be rounded.
+   
+	return Math.round(ctableSqft); //what it returns //it returns what is defined in the function.
+}
+
+var ct= ctableCalc(wcoffeeT,lcoffeeT); //invoking what happens after defining functions...The folded box is two inches wide  and 3 inches long on the horizontal side of the box.
+
+console.log("The amount of space the coffee table will utilize is" + " " + ct + " " + "sqft.");
+}
+        
+else {
+   var ct = 0; //coffee table equals zero
+   alert("You do not have a coffee table in this room.");
+   console.log(ct); 
+	
+}
 
 
 
